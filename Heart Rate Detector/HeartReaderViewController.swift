cc236ed46cb16a941e24c969fa9c22c6ec5e485d
arkrayBergonia <fbergonia.arkray@gmail.com>
2019-10-01 09:55:41 +0800

    added assets

---
+++ Heart Rate Detector/HeartReaderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HeartReaderViewController.swift
+//  Heart Rate Detector
+//
+//  Created by Jay Bergonia on 01/10/2019.
+//  Copyright © 2019 Jay Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class HeartReaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Heart Rate Detector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heart Rate Detector
+//
+//  Created by Jay Bergonia on 01/10/2019.
+//  Copyright © 2019 Jay Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
