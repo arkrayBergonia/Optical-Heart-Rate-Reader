a8d50d44ff9a9a351661d903e8aa4974396f314f
arkrayBergonia <fbergonia.arkray@gmail.com>
2019-10-02 09:25:18 +0800

    added heartRate VC

---
+++ Heart Rate Detector/HeartRateViewController.h
@@ -0,0 +1,17 @@
+//
+//  HeartRateViewController.h
+//  Heart Rate Detector
+//
+//  Created by Jay Bergonia on 02/10/2019.
+//  Copyright © 2019 Jay Bergonia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HeartRateViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Heart Rate Detector/HeartRateViewController.m
@@ -0,0 +1,32 @@
+//
+//  HeartRateViewController.m
+//  Heart Rate Detector
+//
+//  Created by Jay Bergonia on 02/10/2019.
+//  Copyright © 2019 Jay Bergonia. All rights reserved.
+//
+
+#import "HeartRateViewController.h"
+
+@interface HeartRateViewController ()
+
+@end
+
+@implementation HeartRateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
